6c228ea39274610a1602f8481c07588a8f52c307
kk1701 <karankute05@gmail.com>
2024-10-17 22:51:21 +0530

    updated complain controller

--- controllers/complainControllers.js
+++ controllers/complainControllers.js
@@ -1,11 +1,35 @@
 import Complains from "../models/complainModel.js";
 
-const cookieOptions = {
-    secret: true,
-    maxAge: 1*24*60*60*1000,   //1 day
-    httpOnly: true
+// const cookieOptions = {
+//     secret: true,
+//     maxAge: 1*24*60*60*1000,   //1 day
+//     httpOnly: true
+// }
+
+const registerComplain = async (req, res) => {
+    const { complainType, complainDescription, attachments } = req.body
+
+    console.log(complainType, complainDescription, attachments);
+
+    const complain = await Complains.create({
+        complainType,
+        complainDescription,
+        attachments
+    })
+
+    if(!complain){
+        res.status(400).json({
+            success: false,
+            message: "Complain registration failed, please try again."
+        })
+    }
+
+    await complain.save()
+
+    res.status(200).json({
+        success: true,
+        message: "Complain registered successfully!"
+    })
 }
 
-const registerComplain = async (req, res, next) => {
-    const { studentID, studentName, hostelNumber, complainType, complainDescription, }
-}
+export default registerComplain
--- models/complainModel.js
+++ models/complainModel.js
@@ -1,29 +1,29 @@
 import { model, Schema } from 'mongoose'
 
 const complainSchema = new Schema({
-    studentID: {
-        type: String,
-        required: [true, "Student ID is required!"],
-    },
-    studentName: {
-        type: String,
-        required: [true, "Student name is required!"],
-        trim: true
-    },
-    hostelNumber: {
-        type: Intl,
-        required: [true, "Hostel number is required!"],
-    },
+    // studentID: {
+    //     type: String,
+    //     required: [true, "Student ID is required!"],
+    // },
+    // studentName: {
+    //     type: String,
+    //     required: [true, "Student name is required!"],
+    //     trim: true
+    // },
+    // hostelNumber: {
+    //     type: Intl,
+    //     required: [true, "Hostel number is required!"],
+    // },
     complainType: {
         type: String
     },
     complainDescription: {
         type: String,
         trim: true,
-        required: [true, "Description is required!"]
+        // required: [true, "Description is required!"]
     },
     dateReported: {
-        type: Date
+        type: Date,
     },
     status: {
         type: String,
