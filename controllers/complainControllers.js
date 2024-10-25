a535ce71c5ac1a32e2c7b574f960dd5bd1e906f8
Yogananda1504 <vynr1504@gmail.com>
2024-10-25 21:44:19 +0530

    #

--- controllers/complainControllers.js
+++ controllers/complainControllers.js
@@ -1,31 +1,38 @@
 import Complaints from "../models/complainModel.js";
 import appError from "../utils/appError.js";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
+
 const registerComplain = async (req, res, next) => {
-    const { complainType, complainDescription, attachments } = req.body
+	const { complainType, complainDescription, attachments } = req.body;
 
-    console.log(req.body);
-    if (complainType == null || complainDescription == null || attachments == null) {
-        return next(new appError('Please enter all details!', 400))
-    }
+	console.log(req.body);
+	if (
+		complainType == null ||
+		complainDescription == null ||
+		attachments == null
+	) {
+		return next(new appError("Please enter all details!", 400));
+	}
 
-    const complain = await Complaints.create({
-        complainType,
-        complainDescription,
-        attachments
-    })
+	const complain = await Complaints.create({
+		complainType,
+		complainDescription,
+		attachments,
+	});
 
-    if (!complain) {
-        return next(new appError('Complaint registration failed, please try again!', 400))
-    }
+	if (!complain) {
+		return next(
+			new appError("Complaint registration failed, please try again!", 400)
+		);
+	}
 
-    await complain.save()
+	await complain.save();
 
-    res.status(200).json({
-        success: true,
-        message: "Complain registered successfully!"
-    })
-}
+	res.status(200).json({
+		success: true,
+		message: "Complain registered successfully!",
+	});
+};
 
 export default registerComplain;
