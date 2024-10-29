08f4bedf78da1afc7f01f5edb94deda9139146fc
Yogananda1504 <vynr1504@gmail.com>
2024-10-29 22:44:43 +0530

    added update,get,delete complaint routes

--- routes/complainRoutes.js
+++ routes/complainRoutes.js
@@ -1,11 +1,18 @@
-import { Router } from 'express'
-import registerComplain from '../controllers/complainControllers.js'
-import {protect} from '../middleware/protect.js';
+import { Router } from "express";
+import { registerComplain } from "../controllers/complainControllers.js";
+import { protect } from "../middleware/protect.js";
+import { updateComplaints } from "../controllers/complainControllers.js";
+import { getComplaints } from "../controllers/complainControllers.js";
+import { deleteComplaints } from "../controllers/complainControllers.js";
 
-const router = Router()
+const router = Router();
+//Create
+router.post("/register", protect, registerComplain);
+//Read
+router.get("/get-complaints", protect, getComplaints);
+//Update
+router.post("/update-complints", protect, updateComplaints);
+//Delete
+router.delete("/delete-complaints", protect, deleteComplaints);
 
-
-router.post('/register',protect,registerComplain);
-
-
-export default router
+export default router;
