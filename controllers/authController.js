2275ed5724e5bfc5b7025fe17b830a3d9f90a384
Yogananda1504 <123717877+Yogananda1504@users.noreply.github.com>
2024-11-17 20:39:43 +0530

    Update authController.js
    tesing

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,9 +12,7 @@ const authenticator = new Ldap_authenticator(
 const authController = async (req, res, next) => {
 	try {
 		console.log("Request body:", req.body);
-		const { username, password } = req.body
-		
-		
+		const { username, password } = req.body;
 
 		if (!username || !password) {
 			return next(new appError("Username and password are required", 401));
@@ -40,6 +38,46 @@ const authController = async (req, res, next) => {
 			res.status(200).json({
 				success: true,
 				message: "User authenticated successfully",
+				data: {
+					user: {
+						0: {
+							type: "mail",
+							values: ["vynr1504@gmail.com"],
+						},
+						1: {
+							type: "mobile",
+							values: ["7671050452"],
+						},
+						2: {
+							type: "uid",
+							values: ["2211201152"],
+						},
+						3: {
+							type: "cn",
+							values: ["Velpucherla Yogananda Reddy"],
+						},
+						4: {
+							type: "sn",
+							values: ["Velpucherla"],
+						},
+						5: {
+							type: "departmentNumber",
+							values: ["Computer Science and Engineering"],
+						},
+						6: {
+							type: "roomNumber",
+							values: ["H5-C062"],
+						},
+						7: {
+							type: "hostel",
+							values: ["H5"],
+						},
+						role: "student",
+						complaintsRegistered: 0,
+						complaintsResolved: 0,
+						complaintsUnresolved: 0,
+					},
+				},
 			});
 		}
 	} catch (err) {
